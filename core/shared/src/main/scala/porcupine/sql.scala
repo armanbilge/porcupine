200ff400d4057a7cef07fc134bdffd7c9e7174d8
Arman Bilge <armanbilge@gmail.com>
2023-03-04 20:28:36 +0000

    Add `Fragment`+instances and `Query` instances

--- core/shared/src/main/scala/porcupine/sql.scala
+++ core/shared/src/main/scala/porcupine/sql.scala
@@ -16,4 +16,39 @@
 
 package porcupine
 
-final case class Query[A, B](sql: String, encoder: Encoder[A], decoder: Decoder[B])
+import cats.ContravariantMonoidal
+import cats.Monoid
+import cats.arrow.Profunctor
+import cats.syntax.all.*
+
+final class Query[A, B](val sql: String, val encoder: Encoder[A], val decoder: Decoder[B])
+
+object Query:
+
+  given Profunctor[Query] = new:
+    def dimap[A, B, C, D](fab: Query[A, B])(f: C => A)(g: B => D) =
+      Query(fab.sql, fab.encoder.contramap(f), fab.decoder.map(g))
+
+final class Fragment[A](val fragment: String, val encoder: Encoder[A]):
+  def command: Query[A, Unit] = Query(fragment, encoder, Codec.unit)
+
+  def query[B](decoder: Decoder[B]): Query[A, B] = Query(fragment, encoder, decoder)
+
+  def apply(a: A): Fragment[Unit] = Fragment(fragment, encoder.contramap(_ => a))
+
+  def stripMargin: Fragment[A] = Fragment(fragment.stripMargin, encoder)
+  def stripMargin(marginChar: Char): Fragment[A] =
+    Fragment(fragment.stripMargin(marginChar), encoder)
+
+object Fragment:
+  given ContravariantMonoidal[Fragment] = new:
+    val unit = Fragment("", Codec.unit)
+    def product[A, B](fa: Fragment[A], fb: Fragment[B]) =
+      Fragment(fa.fragment + fb.fragment, (fa.encoder, fb.encoder).tupled)
+    def contramap[A, B](fa: Fragment[A])(f: B => A) =
+      Fragment(fa.fragment, fa.encoder.contramap(f))
+
+  given Monoid[Fragment[Unit]] = new:
+    def empty = ContravariantMonoidal[Fragment].unit
+    def combine(x: Fragment[Unit], y: Fragment[Unit]) =
+      (x, y).contramapN(_ => ((), ()))
